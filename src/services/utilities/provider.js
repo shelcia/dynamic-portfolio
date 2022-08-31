c4ffcb25abf99fcf3a7f15d1af01eb463f58de91
shelcia <shelcia1999@gmail.com>
2022-08-31 19:16:47 +0530

    🚀 deployment on aws

--- src/services/utilities/provider.js
+++ src/services/utilities/provider.js
@@ -2,11 +2,10 @@
 
 import axios from "axios";
 import { handleResponse, handleError } from "./response";
-import { VERCEL_BASE_URL } from "../api";
-// import { HEROKU_BASE_URL } from "../api";
+import { AWS_BASE_URL } from "../api";
 
 // Define your api url from any source.
-const BASE_URL = VERCEL_BASE_URL;
+const BASE_URL = AWS_BASE_URL;
 // const BASE_URL = LOCALHOST_URL;
 
 const token = localStorage.getItem("dynamic-token");
