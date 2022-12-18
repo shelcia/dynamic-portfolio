2c00df71402650fc2778f74eafda8b553b83c791
Tushar Banik <107763774+Tushar98644@users.noreply.github.com>
2022-12-18 13:14:10 +0530

    Update template.jsx

--- src/pages/Blog_templates/template.jsx
+++ src/pages/Blog_templates/template.jsx
@@ -113,10 +113,7 @@ const BlogTemplate = () => {
                 <p>Copyrights @2022 Jessica Carter</p>
             </footer>
 
-            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-            <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
-            <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+          
 
 
 
