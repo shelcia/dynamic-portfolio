255b6daff9eda19ca5ea91b464d41d0688b6ffc6
Mrinmoy Kalita <mrin443@gmail.com>
2022-12-19 13:35:55 +0530

    [ADDED]: Signup form validation

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useState } from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Form } from "react-bootstrap";
 import { toast } from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
 import { Pattern1Grad } from "../../components/common/CustomPatterns";
 import { apiAuth } from "../../services/models/AuthModel";
+import * as yup from 'yup'
+import { useFormik } from 'formik'
 
 const Signup = () => {
   const name = useRef("");
@@ -12,12 +14,30 @@ const Signup = () => {
 
   const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: yup.object().shape({
+      name: yup.string()
+        .required("Name is required"),
+      email: yup.string()
+        .email("Must be a valid email")
+        .max(255)
+        .required("Email is required"),
+      password: yup.string()
+        .min(6, "Minimum 6 characters required")
+        .required("Password is required"),
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setLoading(true);
+    }),
+    onSubmit: () => {
+      onSubmit()
+    },
+  });
 
+  const onSubmit = () => {
     toast("We are verifying. Please Wait !!");
 
     const response = {
@@ -34,7 +54,6 @@ const Signup = () => {
         );
         navigate("/login");
       } else {
-        setLoading(false);
         toast.error(res.message);
       }
     });
@@ -48,66 +67,60 @@ const Signup = () => {
             <Col lg={5}>
               <Card className="bg-secondary shadow border-0">
                 <Card.Body className="px-lg-5 py-lg-5">
-                  <form onSubmit={onSubmit}>
-                    <div className="form-group mb-3">
-                      <div className="input-group input-group-alternative">
-                        <input
-                          className="form-control"
-                          placeholder="Name"
-                          ref={name}
-                          minLength="3"
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group mb-3">
-                      <div className="input-group input-group-alternative">
-                        <input
-                          className="form-control"
-                          placeholder="Email"
-                          type="email"
-                          ref={email}
-                          pattern="[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                          minLength="6"
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group focused">
-                      <div className="input-group input-group-alternative">
-                        <input
-                          className="form-control"
-                          placeholder="Password"
-                          type="password"
-                          ref={password}
-                          minLength="6"
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="custom-control custom-control-alternative custom-checkbox">
-                      <input
-                        className="custom-control-input"
-                        id=" customCheckLogin"
-                        type="checkbox"
+                  <Form onSubmit={formik.handleSubmit}>
+                    <Form.Group className="mb-3 position-relative" >
+                      <Form.Label id="1">Name</Form.Label>
+                      <Form.Control
+                        name="name"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        isInvalid={formik.errors.name}
+                        ref={name}
                       />
-                    </div>
-                    <div className="text-center">
-                      {loading ? (
-                        <Button className="my-4" variant="primary" disabled>
-                          Sign up
-                        </Button>
-                      ) : (
-                        <Button
-                          className="my-4"
-                          variant="primary"
-                          type="submit"
-                        >
-                          Sign up
-                        </Button>
-                      )}
-                    </div>
-                  </form>
+                      <Form.Control.Feedback type="invalid" tooltip>
+                        {formik.errors.name}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3 position-relative" >
+                      <Form.Label id="1">Email</Form.Label>
+                      <Form.Control
+                        name="email"
+                        placeholder="name@example.com"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        isInvalid={formik.errors.email}
+                        ref={email}
+                      />
+                      <Form.Control.Feedback type="invalid" tooltip>
+                        {formik.errors.email}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3 position-relative">
+                      <Form.Label id="2">Password</Form.Label>
+                      <Form.Control
+                        name="password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        isInvalid={formik.errors.password}
+                        ref={password}
+                      />
+                      <Form.Control.Feedback type="invalid" tooltip>
+                        {formik.errors.password}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3 mt-5">
+                      <Form.Control 
+                        style={{color: '#FFFFFF'}}
+                        className="bg-blue"
+                        value={"Sign up"}
+                        type="submit"
+                      />
+                    </Form.Group>
+                  </Form>
                 </Card.Body>
               </Card>
               <Row className="mt-3">
