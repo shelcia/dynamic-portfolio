f688961ca8e2e3c51685c1afa527fdb68d38b017
Sattyam <Sattyamsamania2810@gmail.com>
2023-01-01 09:08:18 +0530

    Add Scroll to Top Button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./styles/style.css";
+import  {useState} from 'react';
 
 import routes from "./routes";
 import { useRoutes } from "react-router";
@@ -6,6 +8,25 @@ import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./context/ThemeContext";
 
 const App = () => {
+
+  const [visible, setVisible] = useState(false)
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    } 
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
+  const top = () => {
+
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+      
+    });
+  }
   const allRoutes = useRoutes(routes);
 
   const toasterOptions = {
@@ -15,14 +36,20 @@ const App = () => {
       duration: 3000,
     },
   };
+  window.addEventListener('scroll', toggleVisible);
 
   return (
+    <div className="App">
+      <section className="section1"></section>
+      <section className="section2"></section>
+      <button onClick={top} className="topbtn"> Scroll Up</button>
     <React.Fragment>
       <ThemeProvider>
         <Toaster toastOptions={toasterOptions} />
         {allRoutes}
       </ThemeProvider>
     </React.Fragment>
+    </div>
   );
 };
 
